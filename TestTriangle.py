bfcbe5fed5fbc6788b19fa896991ef934d7d174d
Natalie Bernhard <nbernhard314@gmail.com>
2021-02-15 16:23:30 -0500

    added some tests

--- TestTriangle.py
+++ TestTriangle.py
@@ -26,6 +26,21 @@ class TestTriangles(unittest.TestCase):
     def testEquilateralTriangles(self): 
         self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
 
+    def testGreaterThan200(self):
+        self.assertEqual(classifyTriangle(2,2,400), 'InvalidInput', '2,2,200 should be invalid input')
+        self.assertEqual(classifyTriangle(200,400,400), 'InvalidInput', '200,400,400 should be invalid input')
+        self.assertEqual(classifyTriangle(600,400,500), 'InvalidInput', '600,400,500 should be invalid input')
+
+    def testLessThanOrEqualZero(self):
+        self.assertEqual(classifyTriangle(0,0,0), 'InvalidInput', '0,0,0 should be invalid input')
+        self.assertEqual(classifyTriangle(3,0,3), 'InvalidInput', '3,0,3 should be invalid input')
+        self.assertEqual(classifyTriangle(2,2,-2), 'InvalidInput', '2,2,-2 should be invalid input')
+        self.assertEqual(classifyTriangle(-3,-4,-5), 'InvalidInput', '-3,-4,-5 should be invalid input')
+
+    
+
+    
+
 if __name__ == '__main__':
     print('Running unit tests')
     unittest.main()
